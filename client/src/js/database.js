c2582575a73d8410025a20da461800e6779ebe87
J R <john.richmond1096@gmail.com>
2022-10-22 22:20:06 -0400

    pushing for heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+import { request } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -15,20 +16,23 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content, thisId) => {
   thisId = parseInt(thisId);
+  let recrord = (thisId === 0) ? {content} : {id: thisId, content};
   const db = await openDB('jate');
   const transaction = db.transaction('jate', 'readwrite');
   const store = transaction.objectStore('jate');
-  const request = store.put( content );
+  const request = store.put(record);
   const result = await request;
   console.log('putDb result', result);
+  return result;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await openDB('jate');
-  const transaction = db.transaction('jate');
+  const transaction = db.transaction('jate', 'readonly');
   const store = transaction.objectStore('jate');
-  const result = await store.getAll();
+  const request = await store.getAll();
+  const result = await request;
   console.log('result.value', result);
   return result;
 }
